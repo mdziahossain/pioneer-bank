6ecae8c2b35aa42f60e26272b2b46816939ab1cc
Md Zia Hossain <mdziahossain@yandex.com>
2021-01-26 21:22:54 +0600

    Pioneer Bank v3.0

--- js/main.js
+++ js/main.js
@@ -7,3 +7,47 @@ loginBtn.addEventListener('click', function(){
   // console.log("Clicked");
   transactionArea.style.display = "block";
 });
+
+// Deposit Btn Event Handler
+const depositBtn = document.getElementById("depositBtn");
+depositBtn.addEventListener('click', function(){
+  
+  const depositNumber =  getInputNumber("depositAmount");
+
+  
+  updateSpanText("currentDeposit", depositNumber);
+  updateSpanText("currentBalance", depositNumber);
+
+  document.getElementById("depositAmount").value = "";
+
+});
+
+
+// Withdraw Btn Event Handler
+const withdrawBtn = document.getElementById("withdrawBtn");
+withdrawBtn.addEventListener('click',function(){
+  const withdrawNumber = getInputNumber("withdrawAmount");
+
+
+  updateSpanText("currentWithdraw", withdrawNumber);
+  updateSpanText("currentBalance", -1 * withdrawNumber);
+
+  document.getElementById("withdrawAmount").value = "";
+
+})
+
+function getInputNumber(id){
+  const amount = document.getElementById(id).value;
+  const amountNumber = parseFloat(amount);
+  return amountNumber;
+
+}
+
+
+function updateSpanText(id, depositNumber) {
+  const current = document.getElementById(id).innerText;
+  const currentBalanceNumber = parseFloat(current);
+  const totalAmount = currentBalanceNumber + depositNumber;
+  document.getElementById(id).innerText = totalAmount;
+
+}
